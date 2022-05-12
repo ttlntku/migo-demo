c64c220c963bcc49681f98d5b44f5a0d343efc3c
KIEU <kieungothekieu@gmail.com>
2022-05-12 16:19:27 +0700

    trending: add trending_layout

--- MigoClient/wwwroot/js/site.js
+++ MigoClient/wwwroot/js/site.js
@@ -3,3 +3,16 @@ $('.trending-category-title li').click(function () {
     $(this).closest('.trending-category-title').find('li').removeClass('active');
     $(this).addClass('active');
 });
+
+//swiper
+window.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
